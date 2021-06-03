f4efa916641461dfd6bbe00a5c100d6dafebde1f
brluzn <brl.uzn2017@gmail.com>
2021-06-03 09:06:30 +0300

    spinner added 03.06.2021 09.00

--- app/src/main/java/com/example/coronaapp/MainActivity.java
+++ app/src/main/java/com/example/coronaapp/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.coronaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner_iller;
+    TextView il_vaka_sayisi_textView;
+    TextView il_neleryasak_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner_iller=findViewById(R.id.spinner_iller);
+        il_vaka_sayisi_textView=findViewById(R.id.il_vaka_sayisi_textView);
+        il_neleryasak_text=findViewById(R.id.il_neleryasak_text);
+
+
+        ArrayAdapter<CharSequence> arrayAdapter=ArrayAdapter.createFromResource(this,R.array.cityArrayList,R.layout.style_spinner);
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_iller.setAdapter(arrayAdapter);
+
+        spinner_iller.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        System.out.println(parent.getItemAtPosition(position).toString());
+        il_vaka_sayisi_textView.setText(parent.getItemAtPosition(position).toString()+"  : 88.78");
+        il_neleryasak_text.setText(parent.getItemAtPosition(position).toString());
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
