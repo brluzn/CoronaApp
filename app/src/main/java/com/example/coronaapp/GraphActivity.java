8fec860b74b33516ab34e7665a411860073a9c79
brluzn <brl.uzn2017@gmail.com>
2021-06-08 07:18:50 +0300

    table full added 08.06.2021 08.00

--- app/src/main/java/com/example/coronaapp/GraphActivity.java
+++ app/src/main/java/com/example/coronaapp/GraphActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,16 +18,24 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class GraphActivity extends AppCompatActivity {
+public class GraphActivity extends DrawerActivity {
     public List<turkiye_gunluk> modelclassList;
+
+    TextView table_tarih1;
+         TextView table_vaka,table_iyilesen,table_vefat,table_agirHasta,table_testSayisi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_graph);
-        /*LayoutInflater inflater = LayoutInflater.from(this);
+
+
+
+
+
+        LayoutInflater inflater = LayoutInflater.from(this);
         View view = inflater.inflate(R.layout.activity_graph, null, false);
         drawer.addView(view, 1);
-*/
+
         System.out.println("sdsdsds");
 
 
@@ -52,6 +61,17 @@ public class GraphActivity extends AppCompatActivity {
                 modelclassList =new ArrayList<>(Arrays.asList(d.getData()));
                 System.out.println(modelclassList.get(modelclassList.size()-1).getDate());
 
+                if (modelclassList.size()>0){
+                    Textview_Setups();
+
+                    table_tarih1.setText(modelclassList.get(modelclassList.size()-1).getDate());
+                    table_agirHasta.setText(modelclassList.get(modelclassList.size()-1).getCritical());
+                    table_iyilesen.setText(modelclassList.get(modelclassList.size()-1).getRecovered());
+                    table_testSayisi.setText(modelclassList.get(modelclassList.size()-1).getTests());
+                    table_vaka.setText(modelclassList.get(modelclassList.size()-1).getPatients());
+                    table_vefat.setText(modelclassList.get(modelclassList.size()-1).getDeaths());
+                }
+
             }
 
             @Override
@@ -62,4 +82,14 @@ public class GraphActivity extends AppCompatActivity {
         });
 
     }
+
+    public void Textview_Setups(){
+        table_tarih1=findViewById(R.id.table_tarih1);
+        table_testSayisi=findViewById(R.id.table_testSayisi);
+        table_agirHasta=findViewById(R.id.table_agirhasta);
+        table_iyilesen=findViewById(R.id.table_iyilesen);
+        table_vaka=findViewById(R.id.table_vaka);
+        table_vefat=findViewById(R.id.table_vefat); //textviewlerin eşleştirmesini sagladık
+    }
+
 }
