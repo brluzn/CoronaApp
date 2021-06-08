b79e09d1e2425975d4c431ba734065264ac35c7e
brluzn <brl.uzn2017@gmail.com>
2021-06-08 11:26:03 +0300

    pie chart added 08.06.2021 08.00

--- app/src/main/java/com/example/coronaapp/GraphActivity.java
+++ app/src/main/java/com/example/coronaapp/GraphActivity.java
@@ -2,11 +2,25 @@ package com.example.coronaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,7 +36,7 @@ public class GraphActivity extends DrawerActivity {
     public List<turkiye_gunluk> modelclassList;
 
     TextView table_tarih1;
-         TextView table_vaka,table_iyilesen,table_vefat,table_agirHasta,table_testSayisi;
+    TextView table_vaka,table_iyilesen,table_vefat,table_agirHasta,table_testSayisi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +74,8 @@ public class GraphActivity extends DrawerActivity {
                 // System.out.println(d.getData());
                 modelclassList =new ArrayList<>(Arrays.asList(d.getData()));
                 System.out.println(modelclassList.get(modelclassList.size()-1).getDate());
-
+                BarChart(modelclassList);
+                PieChart(modelclassList);
                 if (modelclassList.size()>0){
                     Textview_Setups();
 
@@ -90,6 +105,67 @@ public class GraphActivity extends DrawerActivity {
         table_iyilesen=findViewById(R.id.table_iyilesen);
         table_vaka=findViewById(R.id.table_vaka);
         table_vefat=findViewById(R.id.table_vefat); //textviewlerin eşleştirmesini sagladık
+    }
+
+    public void BarChart(List<turkiye_gunluk> modelclassList){
+
+        BarChart barChart =findViewById(R.id.barChart);
+
+        ArrayList<BarEntry> visitors=new ArrayList<>();
+
+        for (int i=0;i < modelclassList.size();i++){
+
+            String x=modelclassList.get(i).getDate();
+            int y=Integer.parseInt(modelclassList.get(i).getTotalPatients());
+            visitors.add(new BarEntry(i,y));
+        }
+        BarDataSet barDataSet=new BarDataSet(visitors,"visitor");
+        barDataSet.setColors(Color.GRAY);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(1f);
+
+        BarData barData=new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Türkiye Geneli Hasta Sayisi");
+        barChart.animateY(1000);
+
+    }
+
+    public void PieChart(List<turkiye_gunluk> modelclassList){
+        PieChart pieChart=findViewById(R.id.pieChart);
+
+        int vefat=Integer.parseInt(modelclassList.get(modelclassList.size()-1).getTotalDeaths());
+        int vaka=Integer.parseInt(modelclassList.get(modelclassList.size()-1).getTotalPatients());
+        int iyilesen=Integer.parseInt(modelclassList.get(modelclassList.size()-1).getTotalRecovered());
+
+        int total=vefat+vaka+iyilesen;
+
+
+        ArrayList<PieEntry> entries=new ArrayList<>();
+        entries.add(new PieEntry(vaka,"Aktif Vaka"));
+        entries.add(new PieEntry(iyilesen,"İyilesen"));
+        entries.add(new PieEntry(vefat,"Vefat"));
+
+        PieDataSet pieDataSet=new PieDataSet(entries,"Turkiye");
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData=new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Türkiye Güncel Durum\n"+modelclassList.get(modelclassList.size()-1).getDate());
+        pieChart.animateY(1000, Easing.EaseInCubic);
+
+        pieChart.setDrawHoleEnabled(true);
+        Legend legend = pieChart.getLegend();
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);  //Set the legend horizontal display
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP); //top
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT); //Right to it
+
+
     }
 
 }
